e60c5bbddcfa72e1c5e6c459bda08c8adb462681
95hwang <95chris0319@gmail.com>
2019-07-26 16:35:42 +0900

    updated01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+
 
 class App extends Component {
   render(){
     return(
-      <div className="App">
-        <h1>Brand New Project</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const banner = (props) => {
+	
+	return(
+	<div className="container">
+		
+		<img src="" />
+	</div>
+	)
+}
