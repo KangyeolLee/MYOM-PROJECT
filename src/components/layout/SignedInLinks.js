2004a4923e4844f723bf1acd7d92dbd392d71552
95hwang <95chris0319@gmail.com>
2019-10-08 21:07:11 +0900

    navbar updated

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/authAction';
@@ -15,21 +15,39 @@ class SignedInLinks extends Component {
   }
   render() {
     return (
-      <ul className="right">
-        <li><NavLink to='/providerRegister' className="black-text">판매자 등록</NavLink></li>
-        <li><NavLink to='/chatDashboard' className='black-text'>메시지</NavLink></li>
-        <li><NavLink to='/profile' className='btn btn-floating grey lighten-1'><img src= {this.props.profile.profileImgURL} width="40px" height="40px" className="circle"/></NavLink></li>
-        <li className='dropdown-trigger no-autoinit' data-target='dropdown_mypage'><span className="black-text"style={{display: 'flex'}}>{this.props.profile.initials}님&nbsp;<i className="material-icons">arrow_drop_down</i></span></li>
+      <Fragment>
+        <ul className="right hide-on-small-only">
+          <li><NavLink to='/providerRegister' className="black-text">판매자 등록</NavLink></li>
+          <li><NavLink to='/chatDashboard' className='black-text'>메시지</NavLink></li>
+          <li><NavLink to='/profile' className='btn btn-floating grey lighten-1'><img src= {this.props.profile.profileImgURL} width="40px" height="40px" className="circle"/></NavLink></li>
+          <li className='dropdown-trigger no-autoinit' data-target='dropdown_mypage'><span className="black-text"style={{display: 'flex'}}>{this.props.profile.initials}님&nbsp;<i className="material-icons">arrow_drop_down</i></span></li>
 
-        {/* about dropdown option for UserMyPage */}
-        <ul id="dropdown_mypage" className="dropdown-content">
-          <li><NavLink to='/profile'>계정 설정</NavLink></li>
-          <li><NavLink to='/mypageBuyer/estimate'>마이 페이지</NavLink></li>
-          <li className="divider" tabIndex='-1'></li>
-          <li><a onClick={this.props.signOut}>로그아웃</a></li>
-          <li><NavLink to='/withdrawal'>회원탈퇴</NavLink></li>
+          {/* about dropdown option for UserMyPage */}
+          <ul id="dropdown_mypage" className="dropdown-content">
+            <li><NavLink to='/profile' className="black-text">계정 설정</NavLink></li>
+            <li><NavLink to='/mypageBuyer/estimate' className="black-text">마이 페이지</NavLink></li>
+            <li className="divider" tabIndex='-1'></li>
+            <li><a onClick={this.props.signOut} className="black-text">로그아웃</a></li>
+            <li><NavLink to='/withdrawal' className="black-text">회원탈퇴</NavLink></li>
+          </ul>
         </ul>
-      </ul>
+
+        <ul className="right hide-on-med-and-up">
+          <li><NavLink to='/profile' className='btn btn-floating grey lighten-1'><img src= {this.props.profile.profileImgURL} width="40px" height="40px" className="circle"/></NavLink></li>
+          <li className='dropdown-trigger no-autoinit' data-target='dropdown_mypage2'><span className="black-text"style={{display: 'flex'}}>{this.props.profile.initials}님&nbsp;<i className="material-icons">arrow_drop_down</i></span></li>
+
+          {/* about dropdown option for UserMyPage */}
+          <ul id="dropdown_mypage2" className="dropdown-content">
+            <li><NavLink to='/providerRegister' className="black-text">판매자 등록</NavLink></li>
+            <li><NavLink to='/chatDashboard' className='black-text'>메시지</NavLink></li>
+            <li><NavLink to='/profile' className="black-text">계정 설정</NavLink></li>
+            <li><NavLink to='/mypageBuyer/estimate' className="black-text">마이 페이지</NavLink></li>
+            <li className="divider" tabIndex='-1'></li>
+            <li><a onClick={this.props.signOut} className="black-text">로그아웃</a></li>
+            <li><NavLink to='/withdrawal' className="black-text">회원탈퇴</NavLink></li>
+          </ul>
+        </ul>
+      </Fragment>
     )
   }
 }
